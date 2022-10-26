c51ddcf940ec77ec9e40d9d51f3d762b3df4e53b
sebmar19 <sebmar@eltiempo.com>
2022-10-26 11:35:13 -0500

    Prueba de Commit

---
+++ js/main-mapa.js
@@ -0,0 +1,209 @@
+var comunidades= [
+    {"departamento":"Magdalena",
+"comunidad":"Kogui",
+"procedimiento":"El reglamento interno de los indígenas Kogui especifica dos tipos de justicia:  la de Gultwé u Mukuise (justicia bondadosa) o la de Noanasé y Kashindukay (que es con castigo y humillación). La violación  es considerada una falta grave. Y las sanciones más comunes para este tipo de faltas son confesión ante el mamo, pagamentos en sitios sagrados, trabajo comunitario, encierros y multas económicas.",
+"link":"https://siic.mininterior.gov.co//sites/default/files/upload/SIIC/ReglamentosInternos/reglamento_interno_kagaba-kogui.pdf"
+},
+{"departamento":"La Guajira",
+"comunidad":"Wayuú",
+"procedimiento":"El palabrero es la máxima autoridad judicial de los Wayuú. Su sistema se basa en la compensación de daños. Para el delito de violación, el violador  debe pagar una dote /paünaa/, tasada sobre el monto máximo de ésta y de conformidad con el estátus de la familia. La negativa a pagarla implica una grave falta y puede traer venganzas y retaliaciones.",
+"link":"https://repository.usergioarboleda.edu.co/bitstream/handle/11232/1083/Sistema%20normativo%20Way%C3%BAu.pdf?sequence=1&isAllowed=y"
+},
+{"departamento":"Chocó",
+"comunidad":"Embera, Wounnan, Katío, Chamí y Tule",
+"procedimiento":"En el reglamento  de estas comunidades, la violación y el abuso son considerados problemas sociales graves.“Cuando un hombre viole a una niña, tendrá un castigo de 7 años. Si es a una mujer joven o madura, se castigará por 4 años”. Agrega que “está prohibido entregar (para matrimonio)  niñas menores de 15 años. Los padres que lo hagan, tendrán un castigo de 1 año”.",
+"link":"https://siic.mininterior.gov.co//sites/default/files/upload/SIIC/ReglamentosInternos/reglamento_interno_embera_wounaan.pdf"
+},
+{"departamento":"Antioquia",
+"comunidad":"Embera",
+"procedimiento":"Las sanciones de los emberas incluyen trabajos comunitarios, dinero y cepo (tronco de madera donde se inmovilizan manos y pies). La violación de un padre a su hija es castigada con 10 años de trabajo y cepo.",
+"link":"https://siic.mininterior.gov.co//sites/default/files/upload/SIIC/ReglamentosInternos/ri_atrato.pdf"
+},
+{"departamento":"Arauca",
+"comunidad":"Kichwa",
+"procedimiento":"Los Kichwa castiga las faltas graves con una pena mínima de 3 fuetazos y una multa de entre $60.000 y $80.000. La violación y el abuso sexual están entre las faltas graves que ameritan ese castigo . La autoridad judicial es ejercida por el síndico o Astik.",
+"link":"https://siic.mininterior.gov.co//sites/default/files/upload/SIIC/ReglamentosInternos/ri_la_esmeralda_-_arauca.pdf"
+},
+{"departamento":"Casanare",
+"comunidad":"Kichwa",
+"procedimiento":"La justicia de los Kichwa castiga las faltas graves con una pena mínima de 3 fuetazos y una multa de entre sesenta mil pesos M/cte ($60.000) y ochenta mil pesos M/cte ($80.000). La violación y ell abuso sexual están entre esas faltas graves, que ameritan el castigo mencionado. . La autoridad judicial es ejercida por el síndico o Astik. ",
+"link":"https://siic.mininterior.gov.co//sites/default/files/upload/SIIC/ReglamentosInternos/agua_azul_-_casanare_ri.pdf"
+},
+{"departamento":"Tolima",
+"comunidad":"Paez",
+"procedimiento":'El reglamento de la comunidad Paéz establece que los casos de violación  "se castigarán con cepo, fuete y multa. Si la persona vuelve a cometer el delito se entregará a las autoridades civiles y se le expulsará de la comunidad”.',
+"link":"https://siic.mininterior.gov.co//sites/default/files/upload/SIIC/ReglamentosInternos/ri_del_cabildo_indigena_nataima.pdf"
+},
+{"departamento":"Cauca",
+"comunidad":"Misak",
+"procedimiento":"La justicia Misak tiene cuatro etapas: advertir y aconsejar (en casos leves) , armonizar y corrección física en casos graves, que incluye prisión, fuetazos o cepo. En el caso de las violaciones, los misak coordinan las sanciones con las condenas de la justicia ordinaria.",
+"link":"https://siic.mininterior.gov.co//sites/default/files/upload/SIIC/ReglamentosInternos/ri_guambia_silvia.pdf"
+},
+{"departamento":"Meta",
+"comunidad":"Kichwa",
+"procedimiento":"La justicia de los Kichwa castiga las faltas graves con una pena mínima de 3 fuetazos y una multa de entre sesenta mil pesos M/cte ($60.000) y ochenta mil pesos M/cte ($80.000). La violación y ell abuso sexual están entre esas faltas graves, que ameritan el castigo mencionado. La autoridad judicial es ejercida por el síndico o Astik. ",
+"link":"https://siic.mininterior.gov.co//sites/default/files/upload/SIIC/ReglamentosInternos/ri_la_julia_-_meta.pdf"
+},
+{"departamento":"Vichada",
+"comunidad":"Mokaná, Piapoco, Sikuani, Ocima, Tiatiato, entre otras",
+"procedimiento":"En estas comunidades,  el acceso carnal violento es falta grave: “el que realice acceso carnal con otra persona miembro de la comunidad, que sea menor de 14 años o que emplee la fuerza incurrirá en las sanciones previstas”. En estos casos, la sanción “Se coordina temporalmente con la justicia ordinaria, mientras cada comunidad organiza su sistema judicial”.",
+"link":"https://siic.mininterior.gov.co//sites/default/files/upload/SIIC/ReglamentosInternos/reglamento_interno_del_resguardo_de_puerto_colombia_final_.pdf"
+},
+{"departamento":"Guainía",
+"comunidad":"Puinave",
+"procedimiento":"Los Puinabe establecen que en violación y corrupción de menores “las personas serán objeto de exposición ante la comunidad para la asignación de 10 latigazos y castigo de 12 años sin poder salir de la comunidad, más 12 años de trabajo para la víctima o su familia”.",
+"link":"https://siic.mininterior.gov.co//sites/default/files/reglamento_0.pdf"
+},
+{"departamento":"Putumayo",
+"comunidad":"Awa",
+"procedimiento":"Los Awa contempla delitos graves y muy graves. La violación es considerada muy grave y su sanción es de 15 años de prisión sin rebaja. El abuso sexual es considerado grave y castigado con 12 horas de cepo, 10 fuetazos y 5 salarios mínimos.",
+"link":"https://www.minjusticia.gov.co/programas-co/fortalecimiento-etnico/Documents/banco-2018/5.%20REGLAMENTO%20INTERNO%20NUNALBI%20ALTO%20ULBI-convertido.pdf"
+},
+{"departamento":"Nariño",
+"comunidad":"Pastos",
+"procedimiento":"El Manual de justicia Propia de los Pastos llama “desequilibros” a los delitos. la violación o el intento de hacerlo son  desequilibrio graves y reciben  un castigo mayor cuando la víctima es menor. En esos casos se aplican una o varias de estas  sanciones: cepo (siempre), fuetazos, trabajo comunitario, reclusión en centro comunitario o prisión e indemnización.",
+"link":"https://siic.mininterior.gov.co//sites/default/files/upload/SIIC/ReglamentosInternos/mj_tuquerres.pdf"
+}
+
+]
+
+$(document).ready(function(){
+
+    $("#Botones g").on("click",seleccionarDept);
+    $("#deps").change(selectDepsM);
+    depInicialMobile();
+});
+
+function seleccionarDept(){
+
+    var isMobile =$(".mobile-ui-mapa").css("display")=="block";
+    if (!isMobile){
+
+        $("#Botones g.activo").removeClass("activo");
+        $(this).addClass("activo");
+    
+        var num=$(this).index();
+        console.log(num);
+        $(".titulo-info").fadeOut(300,function(){
+            $(".titulo-info").html(comunidades[num].comunidad);
+            $(".titulo-info").fadeIn(300);
+        });
+    
+        $(".departamento-info").fadeOut(300,function(){
+            $(".departamento-info").html(comunidades[num].departamento);
+            $(".departamento-info").fadeIn(300);
+        });
+        if(!$(".previo-cont").hasClass("oculto")){
+    
+            $(".previo-cont").fadeOut(300,function(){
+                $(".texto-comunidad").html(comunidades[num].procedimiento);
+                $(".comunidad-cont").fadeIn(300);
+                $(".previo-cont").addClass("oculto");
+    
+            });
+    
+        }
+        else{
+            $(".comunidad-cont").fadeOut(300,function(){
+                $(".texto-comunidad").html(comunidades[num].procedimiento);
+                $(".comunidad-cont").fadeIn(300);
+               
+    
+            });
+        }
+
+        $(".link-comunidad a").attr("href",comunidades[num].link);
+    }
+
+    
+}
+
+function depInicialMobile(){
+    var isMobile =$(".mobile-ui-mapa").css("display")=="block";
+    console.log(isMobile);
+    if (isMobile){
+        var num = $("#deps")[0].selectedIndex;
+
+          
+      num = num + 1;
+      
+      $("#Botones g.activo").removeClass("activo");
+      $("#Botones g:nth-child(" + num + ")").addClass("activo");
+      num = num -1;
+      console.log(num);
+      $(".titulo-info").fadeOut(300,function(){
+          $(".titulo-info").html(comunidades[num].comunidad);
+          console.log(comunidades[num].comunidad);
+          $(".titulo-info").fadeIn(300);
+      });
+  
+      $(".departamento-info").fadeOut(300,function(){
+          $(".departamento-info").html(comunidades[num].departamento);
+          $(".departamento-info").fadeIn(300);
+      });
+      if(!$(".previo-cont").hasClass("oculto")){
+  
+          $(".previo-cont").fadeOut(300,function(){
+              $(".texto-comunidad").html(comunidades[num].procedimiento);
+              $(".comunidad-cont").fadeIn(300);
+              $(".previo-cont").addClass("oculto");
+  
+          });
+  
+      }
+      else{
+          $(".comunidad-cont").fadeOut(300,function(){
+              $(".texto-comunidad").html(comunidades[num].procedimiento);
+              $(".comunidad-cont").fadeIn(300);
+             
+  
+          });
+      }
+      $(".link-comunidad a").attr("href",comunidades[num].link);
+    }
+    
+
+}
+
+function selectDepsM(){
+    var num = $(this)[0].selectedIndex;
+      console.log(num);
+
+  
+      num = num + 1;
+      
+      $("#Botones g.activo").removeClass("activo");
+      $("#Botones g:nth-child(" + num + ")").addClass("activo");
+      num = num -1;
+      console.log(num);
+      $(".titulo-info").fadeOut(300,function(){
+          $(".titulo-info").html(comunidades[num].comunidad);
+          console.log(comunidades[num].comunidad);
+          $(".titulo-info").fadeIn(300);
+      });
+  
+      $(".departamento-info").fadeOut(300,function(){
+          $(".departamento-info").html(comunidades[num].departamento);
+          $(".departamento-info").fadeIn(300);
+      });
+      if(!$(".previo-cont").hasClass("oculto")){
+  
+          $(".previo-cont").fadeOut(300,function(){
+              $(".texto-comunidad").html(comunidades[num].procedimiento);
+              $(".comunidad-cont").fadeIn(300);
+              $(".previo-cont").addClass("oculto");
+  
+          });
+  
+      }
+      else{
+          $(".comunidad-cont").fadeOut(300,function(){
+              $(".texto-comunidad").html(comunidades[num].procedimiento);
+              $(".comunidad-cont").fadeIn(300);
+             
+  
+          });
+      }
+      $(".link-comunidad a").attr("href",comunidades[num].link);
+
+  }
